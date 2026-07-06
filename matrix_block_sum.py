4ceb2f7e29e20b3a77513be6eca58a8bd79f8937
Deepak <m16.deepak@outlook.com>
2026-07-06 10:43:37 +0530

    leetcode interview questions

--- matrix_block_sum.py
+++ matrix_block_sum.py
@@ -1,10 +1,8 @@
 def matrixBlockSum(mat, k):
     m, n = len(mat), len(mat[0])
     answer = [[0] * n for _ in range(m)]
-
     for i in range(m):
         for j in range(n):
-            # Calculate the sum for the block centered at (i, j)
             for r in range(max(0, i - k), min(m, i + k + 1)):
                 for c in range(max(0, j - k), min(n, j + k + 1)):
                     answer[i][j] += mat[r][c]
